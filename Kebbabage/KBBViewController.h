6d66e5ee39d7c6e89296d58c6d81943611d77559
Tim Duckett <tim@timduckett.co.uk>
2013-04-06 12:18:40 +0100

    Now plots current location

--- Kebbabage/KBBViewController.h
+++ Kebbabage/KBBViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 #import "KBBNetworkProtocol.h"
 
-@interface KBBViewController : UIViewController <KBBNetworkProtocol>
+@interface KBBViewController : UIViewController <KBBNetworkProtocol, CLLocationManagerDelegate>
 
 @end
--- Kebbabage/KBBViewController.m
+++ Kebbabage/KBBViewController.m
@@ -11,11 +11,15 @@
 #import "FSAClient.h"
 #import "JsonParser.h"
 
-
 @interface KBBViewController ()
 @property (nonatomic, strong) PostcodeClient *postcodeClient;
 @property (nonatomic, strong) FSAClient *fsaClient;
 @property (nonatomic, strong) JsonParser *jsonParser;
+
+@property (nonatomic, strong) CLLocationManager *locManager;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+
 @end
 
 @implementation KBBViewController
@@ -30,6 +34,14 @@
     
     self.fsaClient = [[FSAClient alloc] initWithBaseURL:[NSURL URLWithString:kFSAApiUrl]];
     [self.fsaClient setDelegate:self];
+    
+    self.locManager = [[CLLocationManager alloc] init];
+    [self.locManager setDelegate:self];
+    [self.locManager setDesiredAccuracy:kCLLocationAccuracyBest];
+    
+    [self.locManager startUpdatingLocation];
+    
+    [self.mapView setShowsUserLocation:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -64,6 +76,36 @@
 -(void)handleOutletError:(NSError *)error {
     NSLog(@"error retrieving outlets: %@", error);
 }
+
+#pragma mark -
+#pragma mark CLLocationManager delegate methods
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation {
+    NSLog(@"New latitude: %f", newLocation.coordinate.latitude);
+    NSLog(@"New longitude: %f", newLocation.coordinate.longitude);
+    
+    MKCoordinateRegion region;
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.02;
+    span.longitudeDelta = 0.02;
+    
+    region.span = span;
+    region.center = newLocation.coordinate;
+    
+    [self.mapView setCenterCoordinate:newLocation.coordinate animated:YES];
+    [self.mapView setRegion:region animated:TRUE];
+    [self.mapView regionThatFits:region];
+    
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+    [mapView setCenterCoordinate:userLocation.location.coordinate animated:YES];
+}
+
+#pragma mark -
+#pragma mark MKMapView delegate methods
 
 
 
